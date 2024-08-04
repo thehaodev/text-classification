29986a87995da138cd59b99581e634f96fdc2afc
DESKTOP-Q9GD575\PC <thehaodev96@gmail.com>
2024-08-04 08:09:59 +0700

    Data preprocessing

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import string
 import nltk
+import utils_data as data
+
 nltk.download('stopwords')
 nltk.download('punkt')
 import pandas as pd
 import numpy as np
-import matplotlib . pyplot as plt
+import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
 
+# Prepare dataset
 DATASET_PATH = '2cls_spam_text_cls.csv'
 df = pd.read_csv(DATASET_PATH)
 
 messages = df["Message"].values.tolist()
 labels = df['Category'].values.tolist()
+
+# Data preprocessing
+messages = [data.preprocess_text(message) for message in messages]
+dictionary = data.create_dictionary(messages)
+X = np.array([data.create_features(tokens, dictionary) for tokens in
+              messages])
